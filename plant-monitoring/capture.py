ee963366895afc4e57e5bef886356cbc3a52845c
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2025-04-30 21:29:54 +0700

    fix: file save output

--- plant-monitoring/capture.py
+++ plant-monitoring/capture.py
@@ -10,9 +10,11 @@ def capture_image():
         print("Failed to capture image")
         return None
 
+    base_path = "/home/dhanikalisa/projects/python/if4051_smart-farming/plant-monitoring"
+    image_dir = os.path.join(base_path, "images")
     os.makedirs('images', exist_ok=True)
     filename = datetime.now().strftime("%Y%m%d_%H%M%S.jpg")
-    filepath = os.path.join('images', filename)
+    filepath = os.path.join(image_dir, filename)
     cv2.imwrite(filepath, frame)
     cam.release()
     print(f"Image saved as {filepath}")
